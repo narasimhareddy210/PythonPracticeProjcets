3b8a6ef2e26d6325822ab0be44fa007b6b8ad146
Narasiha Reddy <narasimhareddy210@gmail.com>
2019-05-24 21:50:42 +0530

    The a brake
    Time brake using while loop.

---
+++ Take_A_Break.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+total_breaks = 5
+break_count =0
+print("This program started on "+time.ctime())
+while(break_count < total_breaks):
+    time.sleep(213)
+    webbrowser.open("https://youtu.be/8aRor905cCw")
+    break_count = break_count + 1;
