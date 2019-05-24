9611b5ce29686a5dec15ec1f1d191b54501a800d
Narasiha Reddy <narasimhareddy210@gmail.com>
2019-05-24 21:54:11 +0530

    Update Take_A_Break.py

--- Take_A_Break.py
+++ Take_A_Break.py
@@ -4,6 +4,6 @@ total_breaks = 5
 break_count =0
 print("This program started on "+time.ctime())
 while(break_count < total_breaks):
-    time.sleep(213)
     webbrowser.open("https://youtu.be/8aRor905cCw")
-    break_count = break_count + 1;
+    break_count = break_count + 1
+    time.sleep(213)
